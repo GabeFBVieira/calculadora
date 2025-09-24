8399b5f99425c359b7fba71cbc0bcdb1c4f64e04
Gabriel Vieira <gabriel12.leo@gmail.com>
2025-09-23 23:01:27 -0300

    update tests

--- calculadora.py
+++ calculadora.py
@@ -14,55 +14,54 @@ def potenciacao(num1,num2):
         
     return num1 ** num2
 
-if __name__ == '__main__':
-  
+def escolher_operacao(num1, num2, escolha):
+   
+    try:
+        num1_float = float(num1)
+        num2_float = float(num2)
+    except ValueError:
+        return "Um ou ambos os números digitados são inválidos"
+
+    operadores_permitidos = '12345'
+    if escolha not in operadores_permitidos:
+        return "Operador inválido, tente novamente"
+
+    if escolha == '1':
+        return soma(num1_float, num2_float)
+    elif escolha == '2':
+        return subtracao(num1_float, num2_float)
+    elif escolha == '3':
+        return multiplicacao(num1_float, num2_float)
+    elif escolha == '4':
+        return divisao(num1_float, num2_float)
+    elif escolha == '5':
+        return potenciacao(num1_float, num2_float)
+
+
+if __name__ == "__main__":  
     while True: 
+                
+            print('-----------------------')
+            print('      Calculadora       ')
+            print(' 1.Soma\n 2.Subtração\n 3.Multiplicação\n 4.Divisão\n 5.Potenciação\n 6.Sair')
+            print('----------------------')
             
-        print('-----------------------')
-        print('      Calculadora       ')
-        print('1.Soma\n 2.Subtração\n 3.Multiplicação\n 4.Divisão\n 5.Potenciação\n 6.Sair')
-        print('----------------------')
+            num1 = int(input('Digite um Número: '))
+            num2 = int(input('Digite um Número: '))
+            escolha = input('Qual operação deseja executar? Escolha um número de 1 a 5  ')
+
+            resultado = escolher_operacao(num1, num2, escolha)
+            print('Resultado:', resultado)
+
+            sair= (input('Deseja sair? [s]im ou [n]ão ').lower().startswith('s'))
+                
+            if sair is True:
+                print('Saindo...')
+                break
+            else:
+                
+                continue
+
+
         
-        num1 = int(input('Digite um Número: '))
-        num2 = int(input('Digite um Número: '))
-        escolha = input('Qual operação deseja executar? Escolha um número de 1 a 5  ')
-        
-        numeros_validos = None
-        
-        try:
-            num1_float = float(num1)
-            num2_float = float(num2)
-            numeros_validos = True
-        except:
-            numeros_validos = None
-        
-        if numeros_validos is None:
-            print('Um ou ambos os números digitados são invalidos')
-            continue
-        
-        operadores_permitidos = '12345'
-        
-        if escolha not in operadores_permitidos:
-            print('operador inválido, tente novamente')
-            continue
-        
-        if escolha == '1':
-            print(soma(num1_float,num2_float))
-        elif escolha == '2':
-            print(subtracao(num1_float,num2_float))
-        elif escolha == '3':
-            print(multiplicacao(num1_float,num2_float))
-        elif escolha == '4':
-            print(divisao(num1_float,num2_float))
-        elif escolha == '5':
-            print(potenciacao(num1_float,num2_float)) 
-        
-        
-        sair= (input('Deseja sair? [s]im ou [n]ão ').lower().startswith('s'))
-            
-        if sair is True:
-            print('Saindo...')
-            break
-        else:
-            
-            continue
+
--- tests.py
+++ tests.py
@@ -1,24 +1,32 @@
 import unittest
 
-from calculadora import soma,subtracao,divisao,multiplicacao,potenciacao
+from calculadora import soma,subtracao,divisao,multiplicacao,potenciacao, escolher_operacao
 
 
 class TestCalculadora(unittest.TestCase):
     def test_soma(self):
         self.assertEqual(soma(10,5),15)
         self.assertEqual(soma(5,1),6)
+        self.assertEqual(soma(0,0),0)
+        self.assertEqual(soma(5,0),5)
 
     def test_subtracao(self):
         self.assertEqual(subtracao(5,5),0)
         self.assertEqual(subtracao(1,3),-2)
+        self.assertEqual(subtracao(-1,0),-1)
+        self.assertEqual(subtracao(-10,4),-14)
     
     def test_divisao(self):
         self.assertEqual(divisao(2,2),1)
         self.assertEqual(divisao(4,2),2)
+        self.assertEqual(divisao(8,-1),-8)
+        
     
     def test_multiplicacao(self):
         self.assertAlmostEqual(multiplicacao(10,10),100)
         self.assertAlmostEqual(multiplicacao(50,2),100)
+        self.assertAlmostEqual(multiplicacao(-10,50),-500)
+        self.assertEqual(multiplicacao(10,0),0)
     
     def test_potenciacao(self):
         self.assertAlmostEqual(potenciacao(2,10),1024)
@@ -26,8 +34,13 @@ class TestCalculadora(unittest.TestCase):
 
     def test_divisao_por_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            divisao(0,0)
-
+            divisao(5,0)
+    
+    def test_escolha(self):
+        self.assertEqual(escolher_operacao('7','8','9'),'Operador inválido, tente novamente')
+    
+    def test_numero(self):
+        self.assertEqual(escolher_operacao('a','b','c'),'Um ou ambos os números digitados são inválidos')
 
 if __name__ == '__main__':
     unittest.main()
